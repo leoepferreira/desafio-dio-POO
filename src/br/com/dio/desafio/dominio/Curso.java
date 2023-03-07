ff764bafa283008266266a7977480ff349f2eb7b
Leonardo Ferreira <leoepferreira@hotmail.com>
2023-03-06 23:31:09 -0300

    Mentoria e Curso

---
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -0,0 +1,28 @@
+package br.com.dio.desafio.dominio;
+
+public class Curso {
+    private String titulo;
+    private String descricao;
+    private int cargaHoraria;
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    @Override
+    public String toString() {
+        return "Curso{" +
+                "titulo='" + titulo + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", cargaHoraria=" + cargaHoraria +
+                '}';
+    }
+}
